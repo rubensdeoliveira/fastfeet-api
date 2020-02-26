ef5521ef62b685bb884364dd0e4fd5d9fef5a1a1
rubensojunior <rubensojunior6@gmail.com>
2020-02-26 18:02:18 -0300

    adding eslint, prettier and editorconfig

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
-import express from 'express'
+import express from 'express'
 
-import routes from './routes'
+import routes from './routes'
 
-class App {
+class App {
+  constructor() {
+    this.server = express()
 
-constructor() {
+    this.middlewares()
 
-this.server = express()
+    this.routes()
+  }
 
-this.middlewares()
+  middlewares() {
+    this.server.use(express.json())
+  }
 
-this.routes()
-
-}
-
-middlewares() {
-
-this.server.use(express.json())
-
-}
-
-routes() {
-
-this.server.use(routes)
-
-}
-
+  routes() {
+    this.server.use(routes)
+  }
 }
 
 export default new App().server

8c4abbaaa08feae02f7b2f8a0a740f917effffa8
rubensojunior <rubensojunior6@gmail.com>
2020-04-03 17:59:44 -0300

    adding yup validation to recipient controller

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,13 +1,44 @@
+import * as Yup from 'yup'
 import Recipient from '../models/Recipient'
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .required()
+        .min(6),
+      street: Yup.string().required(),
+      neighborhood: Yup.string().required(),
+      number: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      complement: Yup.string(),
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' })
+    }
+
     const recipient = await Recipient.create(req.body)
 
     return res.json(recipient)
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().min(6),
+      street: Yup.string(),
+      neighborhood: Yup.string(),
+      number: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      complement: Yup.string(),
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' })
+    }
+
     const recipient = await Recipient.findByPk(req.params.id)
 
     if (!recipient) {
